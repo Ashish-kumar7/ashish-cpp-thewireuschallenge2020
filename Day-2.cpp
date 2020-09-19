7086f4d1982ac2c18e96f96ed1271cba700015c7
ashish007kumar <ashishkumar357ak@gmail.com>
2020-09-19 19:33:46 +0530

    Day-2

---
+++ Day-2.cpp
@@ -0,0 +1,21 @@
+// author: Ashish Kumar
+// day: 2
+// #thewireuschallenge2020
+// c++
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    string str;
+    cin>>str;
+    cout<<str[0];
+    
+    for(int i=1;i<str.length();i++)
+    {
+        if(isupper(str[i])){
+            cout<<endl;
+        }
+        cout<<str[i];
+    }
+    return 0;
+}

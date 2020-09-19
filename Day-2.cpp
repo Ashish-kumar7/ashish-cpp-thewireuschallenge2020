33750a3419635cfef9856a10ef16ef4194a0361c
ashish007kumar <ashishkumar357ak@gmail.com>
2020-09-19 19:38:02 +0530

    day2

--- Day-2.cpp
+++ Day-2.cpp
@@ -11,11 +11,11 @@ int main()
     cin>>str;
     cout<<str[0];
     
-    for(int i=1;i<str.length();i++)
-    {
+    for(int i=1;i<str.length();i++){
         if(isupper(str[i])){
             cout<<endl;
         }
+
         cout<<str[i];
     }
     return 0;

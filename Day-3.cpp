e84f6c4866c5dd0cac31d57b3aa1e5153eaf0644
ashish007kumar <ashishkumar357ak@gmail.com>
2020-09-20 19:15:41 +0530

    day-3

---
+++ Day-3.cpp
@@ -0,0 +1,69 @@
+// author: Ashish Kumar
+// day: 3
+// #thewireuschallenge2020
+// c++
+
+#include<bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+
+	    long long int n;
+	    cin>>n;
+	    long long int A[n];
+	    for(int i=0;i<n;i++){
+	        cin>>A[i];
+	    }
+	 stack<long long int>S;
+	 long long int area=0;
+	long long int max_area=0;
+	long long  int i;
+	 for( i=0;i<n;)
+	 {
+	    if(S.empty() || A[S.top()]<=A[i]){
+	     S.push(i);
+	     i++;
+	     //cout<<S.size()<<" ";
+	    }
+	    else{
+	        int top=S.top();
+	       // cout<<top<<endl;
+	        S.pop();
+	        
+	        if(S.empty())
+	        {
+	            area=A[top] * i;
+	        }
+	        else{
+	            area=A[top] * (i-S.top()-1);
+	        }
+	        if(max_area<area)
+	        {
+	            max_area=area;
+	        }
+	    }
+	 }
+	 while(!S.empty())
+	 {
+	     long long int top=S.top();
+	        S.pop();
+	        
+	        if(S.empty())
+	        {
+	            area=A[top] * i;
+	        }
+	        else{
+	            area=A[top] * (i-S.top()-1);
+	        }
+	        if(max_area<area)
+	        {
+	            max_area=area;
+	        }
+	 }
+	    
+	    cout<<max_area<<endl;
+	    
+	 
+	return 0;
+}
